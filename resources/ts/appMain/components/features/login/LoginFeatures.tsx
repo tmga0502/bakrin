f5e2c3a4b899a7651128221a8ed95886049027f9
tmga0502 <tmga0502$gmail.com>
2024-06-03 11:59:11 +0900

    ログインフォーム修正

--- resources/ts/appMain/components/features/login/LoginFeatures.tsx
+++ resources/ts/appMain/components/features/login/LoginFeatures.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import loginImage from "@/images/loginImage.jpg";
 import logo from "@/images/logo.png";
-import {FormGroup, FormLabel} from "@/ts/appMain/components/_ui/form/Form";
+import {InputField} from "@/ts/appMain/components/_ui/form/Form";
 import {MainButton} from "@/ts/appMain/components/_ui/button/Button";
-import {useForm} from "react-hook-form";
+import {FormProvider, useForm} from "react-hook-form";
 import {useLogin} from "@/ts/_api/query/AuthQuery";
 
-type FormType = {
-    email: string,
-    password: string
-}
-
 const LoginFeatures = () => {
-
-    const { register, handleSubmit, formState: { errors } } = useForm<FormType>()
     const login = useLogin()
-    const onSubmit = handleSubmit((data) => {
-        // setIsLoading(true)
+    const methods = useForm();
+    const { handleSubmit} = methods;
+
+    const onSubmit = (data:any) => {
         login.mutate(data)
-    })
+    }
 
     return (
         <div className="flex justify-center items-center h-screen bg-darkGreen">
@@ -31,21 +26,15 @@ const LoginFeatures = () => {
                 </div>
                 <div className="w-full p-8 order-2 sm:w-1/2 sm:order-1">
                     <h1 className="text-xl  sm:text-2xl font-bold mb-8 text-center">ログイン</h1>
-                    <form onSubmit={onSubmit}>
-                        <FormGroup>
-                            <FormLabel label={'メールアドレス'} for={'email'}/>
-                            <input className={'inputStyle'} id={'email'} {...register('email', {required:true})}/>
-                            {errors.email && (<p className="text-danger text-sm">入力必須です</p>)}
-                        </FormGroup>
-                        <FormGroup>
-                            <FormLabel label={'パスワード'} for={'password'}/>
-                            <input type={'password'} className={'inputStyle'} id={'password'} {...register('password',{required:true})}/>
-                            {errors.password && (<p className="text-danger text-sm">入力必須です</p>)}
-                        </FormGroup>
-                        <div className="mt-8">
-                            <MainButton value={'ログイン'} color={'success'} type={'submit'} width={'full'}/>
-                        </div>
-                    </form>
+                    <FormProvider {...methods}>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <InputField label={'メールアドレス'} id={'email'} type={'text'} required={true}/>
+                            <InputField label={'パスワード'} id={'password'} type={'password'} required={true}/>
+                            <div className="mt-8">
+                                <MainButton value={'ログイン'} color={'success'} type={'submit'} width={'full'}/>
+                            </div>
+                        </form>
+                    </FormProvider>
                 </div>
             </div>
         </div>
