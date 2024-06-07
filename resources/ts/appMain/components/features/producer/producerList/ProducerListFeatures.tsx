eca18aa767e36e4c6fc392d4f1b5a2e74a5b5d37
tmga0502 <tmga0502$gmail.com>
2024-06-07 10:27:00 +0900

    生産者一覧ページ

--- resources/ts/appMain/components/features/producer/producerList/ProducerListFeatures.tsx
+++ resources/ts/appMain/components/features/producer/producerList/ProducerListFeatures.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import {PageTitle} from "@/ts/appMain/components/_ui/title/Title";
 import Breadcrumb from "@/ts/appMain/components/_common/breadcrumb/Breadcrumb";
 import {MainLayout} from "@/ts/appMain/components/_common/layout/Layout";
-import ItemList from "@/ts/appMain/components/features/producer/producerList/container/producerList/ProducerList";
+import ProducerList from "@/ts/appMain/components/features/producer/producerList/container/producerList/ProducerList";
+import {useGetPopularProducers} from "@/ts/_api/query/ProducerQuery";
+import Loader from "@/ts/appMain/components/_common/loader/Loader";
 
 const ProducerListFeatures = () => {
-    return (
-        <MainLayout>
-            <PageTitle en={'PRODUCERS'} jp={'生産者一覧'}/>
-            <Breadcrumb/>
-            <ItemList />
-        </MainLayout>
-    );
+	const { data: producersData, status } = useGetPopularProducers()
+
+	if (status === 'success'){
+		return (
+			<MainLayout>
+				<PageTitle en={'PRODUCERS'} jp={'生産者一覧'}/>
+				<Breadcrumb/>
+				<ProducerList data={producersData}/>
+			</MainLayout>
+		);
+	}else{
+		return <Loader/>
+	}
 };
 
 export default ProducerListFeatures;
--- resources/ts/appMain/components/features/producer/producerList/container/producerList/ProducerList.tsx
+++ resources/ts/appMain/components/features/producer/producerList/container/producerList/ProducerList.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import {ProducerPanel} from "@/ts/appMain/components/_common/imageListItem/ImageListItem";
 import {ProducerType} from "@/ts/types/ProducerType";
 
-const ProducerList = (data:any) => {
+const ProducerList = (props:{data:ProducerType[]}) => {
     return (
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-            {data.map((producer: ProducerType) => (
+            {props.data.map((producer: ProducerType) => (
                 <ProducerPanel data={producer} key={producer.id}/>
             ))}
         </div>
