9072e55b1ce6fcf3709c5939a561ec46dd571056
tmga0502 <tmga0502$gmail.com>
2024-06-10 09:04:38 +0900

    【欲しいもの】の追加

--- resources/ts/appMain/components/features/myPage/Index/MyPageFeatures.tsx
+++ resources/ts/appMain/components/features/myPage/Index/MyPageFeatures.tsx
@@ -9,6 +9,7 @@ import {FaRegArrowAltCircleRight} from "react-icons/fa";
 
 const mypageMenu = [
     {name:'お気に入り', link:'myfavorite'},
+    {name:'欲しいもの', link:''},
     {name:'交換履歴', link:'tradeHistory'},
     {name:'請求書', link:'invoice'},
     {name:'基本情報', link:'profile'},
