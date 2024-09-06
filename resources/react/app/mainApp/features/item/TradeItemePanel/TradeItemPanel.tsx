bfa7e8f319f5f5241e476f3d251c59c40fe0a067
tmga0502 <tmga0502$gmail.com>
2024-09-06 22:25:42 +0900

    ディレクトリ構成変更

--- resources/react/app/mainApp/features/item/TradeItemePanel/TradeItemPanel.tsx
+++ resources/react/app/mainApp/features/item/TradeItemePanel/TradeItemPanel.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import { TradeItemPanelType } from './_type';
 
-const TradeItemPanel: React.FC<TradeItemPanelType> = ({link, thumbnail, itemName, producerName, date, status}) => {
+const TradeItemPanel: React.FC<TradeItemPanelType> = ({link, thumbnail, itemName, producerName, date}) => {
 	return (
 		<Link to={link} className="flex items-center gap-4 max-w-[500px] min-w-[350px] mx-auto border rounded-md bg-white shadow mb-4 p-4">
 			<div className="w-[120px] h-[120px]">
