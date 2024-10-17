45dedb7c6394dd6e47a97dd0a221a6db3edc4424
tmga0502 <tmga0502$gmail.com>
2024-10-18 06:00:59 +0900

    アイテム詳細表示変更

--- resources/react/app/mainApp/features/item/components/ItemDescription/ItemDescription.tsx
+++ resources/react/app/mainApp/features/item/components/ItemDescription/ItemDescription.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import {ItemName, DescriptionWrapper, DescriptionItem, ColorRed} from './ItemDescription.styles';
-import {Plan} from "@/react/_constants/Plan";
 import {ItemDataType} from "@mainFeatures/item/types";
+import ItemPlanBadge from "@mainElements/budge/ItemPlanBadge/ItemPlanBadge";
 
 const ItemDescription: React.FC<ItemDataType> = ({itemData}) => {
-	const plan = Plan.find(plan => plan.number === itemData.planId)
-	const planName = plan ? plan.name : '';
 
 	return (
 		<div>
 			<div css={ItemName}>{itemData.name}</div>
 			<div css={DescriptionWrapper}>
-				<p css={DescriptionItem}>{planName}</p>
+				<p css={DescriptionItem}>
+					<ItemPlanBadge plan={itemData.plan}/>
+				</p>
+				<p css={DescriptionItem}>{`${itemData.category.name} / ${itemData.variety.name}`}</p>
+				<p css={DescriptionItem}>{`数量：${itemData.count}${itemData.unit.name}`}</p>
 				<p css={DescriptionItem}>{`数量：${itemData.count}${itemData.unit.name}`}</p>
 				<p css={[DescriptionItem, ColorRed]}>{`数量目安：${itemData.guideCount}${itemData.guide_unit.name}`}</p>
 
